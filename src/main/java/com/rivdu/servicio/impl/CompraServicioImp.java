02d10a32eb220a8b96baed0efd9515ab25dae45a
MarioMario <marioramos083@gmail.com>
2018-03-27 10:01:21 -0500

    hola que hace

--- src/main/java/com/rivdu/servicio/impl/CompraServicioImp.java
+++ src/main/java/com/rivdu/servicio/impl/CompraServicioImp.java
@@ -184,6 +184,9 @@ public class CompraServicioImp extends GenericoServicioImpl<Compra, Long> implem
         if (correlativo!= null && !correlativo.equals("")) {
             filtro.add(Restrictions.ilike("c.correlativo1",'%'+correlativo+'%'));
         }
+        busquedaPaginada.setTotalRegistros(compraDao.cantidadPorCriteria(filtro, "c.id"));
+        busquedaPaginada.calcularCantidadDePaginas();
+        busquedaPaginada.validarPaginaActual();
         filtro.setProjection(Projections.projectionList()
                 .add(Projections.distinct(Projections.property("p.id")))
                 .add(Projections.property("c.id"), "id")
